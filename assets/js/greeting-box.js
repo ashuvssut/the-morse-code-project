8f0dade6cd95415b53ad8b4c338bfab745455ab7
ashuvssut <ashu.khanduala@gmail.com>
2020-06-02 22:50:40 +0530

    greeting  box done

---
+++ assets/js/greeting-box.js
@@ -0,0 +1,7 @@
+//Task Pending
+//  1.  Add a functionality that closes the sidebar when you 
+//      click and area outside the sidebar
+
+function closeGreeting(){
+    document.querySelector('.greeting-box').classList.toggle('close');
+}
