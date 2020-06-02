7912f9480f6af8f21cb7045f4c1796e38940f956
ashuvssut <ashu.khanduala@gmail.com>
2020-06-02 19:14:35 +0530

    sidebar added

---
+++ assets/js/sidebar.js
@@ -0,0 +1,8 @@
+//Task Pending
+//  1.  Add a functionality that closes the sidebar when you 
+//      click and area outside the sidebar
+
+function toggleSidebar(){
+     document.querySelector('.main-menu').classList.toggle('show');
+}
+
